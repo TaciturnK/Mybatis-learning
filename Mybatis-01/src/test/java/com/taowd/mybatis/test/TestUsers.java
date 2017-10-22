9ce82c86dfa2a7f8d7be3d6d082fa86cdb8e6a5f
Taowd <taowd@outlook.com>
2017-10-22 15:44:32 +0800

    mybatis调用存储过程学习

--- Mybatis-01/src/test/java/com/taowd/mybatis/test/TestUsers.java
+++ Mybatis-01/src/test/java/com/taowd/mybatis/test/TestUsers.java
@@ -1,6 +1,8 @@
 package com.taowd.mybatis.test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
@@ -24,5 +26,17 @@ public class TestUsers {
 			logger.info("影响行数：" + user.toString());
 		}
 
+	}
+
+	@Test
+	public void testGetUserSexCount() {
+		// 注意此处默认不是自动提交事务的
+		SqlSession session = MybatisUtils.getFactory().openSession(true);// 创建自动提交事物的Session对象
+		String sql = "mapper.userMapper.getUserCount";
+		Map<String, Integer> paramMap = new HashMap<>();
+		paramMap.put("sex_id", 2);
+		session.selectOne(sql, paramMap);
+		Integer userCount = paramMap.get("user_count");
+		logger.info("满足条件的人数：" + userCount);
 	}
 }
